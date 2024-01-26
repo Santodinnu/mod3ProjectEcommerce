b7f63788948b68e292e56dde2b17e3f05cfedcf4
santodinnu <shantoshi@gmail.com>
2024-01-26 14:21:36 -0500

    imported offers in Shop.jsx

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,5 +1,8 @@
 import Hero from "../Components/Hero/Hero"
 import Popular from "../Components/Popular/Popular"
+import Offers from "../Components/Offers/Offers"
+
+
 const Shop = () => {
 
     
@@ -9,6 +12,7 @@ const Shop = () => {
       <div>
         <Hero />
         <Popular />
+        <Offers />
       </div>
     )
   }
