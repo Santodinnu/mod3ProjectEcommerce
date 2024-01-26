e33b075e05f5cef04849bd84cc40192bd348ca44
santodinnu <shantoshi@gmail.com>
2024-01-26 11:52:36 -0500

    added login button and cart icon

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -19,8 +19,12 @@ const Navbar = () => {
                 <li>Men</li>
                 <li>Women</li>
                 <li>Kids</li>
-
             </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="cart"/>
+
+            </div>
 
             
         </div>
