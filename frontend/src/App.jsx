330f98f13a1fa4e960e45884b75ce64df1603789
santodinnu <shantoshi@gmail.com>
2024-01-26 12:39:06 -0500

    set routes in app.jasx for the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   
 
   return (
     <>
+     <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop gender="all" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup/>} />
+        </Routes>
+        <Footer />
+      </Router>
      <Navbar />
     </>
   )
