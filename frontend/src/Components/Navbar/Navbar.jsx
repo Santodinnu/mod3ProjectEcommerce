552778c55f19d39c61a84c5f8d9c8c4053609692
santodinnu <shantoshi@gmail.com>
2024-01-26 12:09:48 -0500

    added state variable to change the active state of navbar items

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
@@ -8,6 +8,8 @@ import cart_icon from '../Assets/cart_icon.png'
 
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop")
     return (
         <div className="navbar">
             <div className="nav-logo">
@@ -23,6 +25,7 @@ const Navbar = () => {
             <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cart_icon} alt="cart"/>
+                <div className="nav-cart-count">0</div>
 
             </div>
 
