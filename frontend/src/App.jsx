148e2a8c22047b84d1630811605ffd29bc356fbd
santodinnu <shantoshi@gmail.com>
2024-01-26 12:48:23 -0500

    imorted and added link to menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
+import Product from "./Pages/Product";
+// import Footer from "./Components/Footer/Footer";
+import ShopCategory from "./Pages/ShopCategory";
+import women_banner from "./Components/Assets/banner_women.png";
+import men_banner from "./Components/Assets/banner_mens.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
+import LoginSignup from "./Pages/LoginSignup";
+
 function App() {
-  
 
   return (
-    <>
-     <Router>
+    <div>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop gender="all" />} />
@@ -19,11 +27,10 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup/>} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
-     <Navbar />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
